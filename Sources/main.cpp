82a7a7770a9ba4e0808cd314301ba998aac9e232
Kamil <kamillys@gmail.com>
2014-06-09 23:50:46 +0200

    Missing ; added

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -6,7 +6,7 @@
 #include "in_scene.h"
 #include "util.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]);
 #ifdef WIN32
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow);
 #endif
